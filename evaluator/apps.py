232a08d9fd3446ab47861d4d60e44f821b685d8a
MaryVH08 <marycarmen.vives@aneiap.co>
2025-04-18 19:20:44 -0500

    Update apps.py

--- evaluator/apps.py
+++ evaluator/apps.py
@@ -1,1 +1,6 @@
+from django.apps import AppConfig
 
+class EvaluatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'evaluator'
+    verbose_name = 'Evaluador de Hojas de Vida ANEIAP'
