7ee1beb5723274ebc4fb045ad1b5b780b6b484ec
MaryVH08 <marycarmen.vives@aneiap.co>
2025-04-18 19:04:58 -0500

    Update urls.py

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path
+from . import views
 
+app_name = 'evaluator'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('simple/', views.evaluate_simple, name='simple'),
+    path('descriptive/', views.evaluate_descriptive, name='descriptive'),
+    path('report/<int:pk>/', views.view_report, name='view_report'),
+    path('download/<int:pk>/', views.download_report, name='download_report'),
+    path('list/', views.evaluation_list, name='list'),
+]
